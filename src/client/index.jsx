68ed8ec216fcd360d5bca58797d0d28621063d85
Parvathi Vallinayagam <parvathi.vallinayagam@mrcooper.com>
2020-11-25 13:59:13 +0530

    Add basic react code

---
+++ src/client/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./javascripts/Components/App";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("my-react-boilerplate")
+);
---
+++ src/client/javascripts/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div>Lets make this boilerplate ready!!</div>
+    </div>
+  );
+};
+
+export default App;
